9f18dd74dd60dd8c5671260c66e8d59f63f9c3b0
WhoisBsa <mbscbsjbs@gmail.com>
2019-04-17 21:58:39 -0300

    Site Pessoal

--- js/main.js
+++ js/main.js
@@ -9,8 +9,6 @@ $(window).scroll(function() {
 });
 
 $(document).on('click', 'a', function(event){
-    event.preventDefault();
-
     $('html, body').animate({
         scrollTop: $( $.attr(this, 'href') ).offset().top
     }, 1250);
