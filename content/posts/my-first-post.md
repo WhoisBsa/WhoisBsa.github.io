1d00b2e489da3623177eb0a0af021621a5d8854e
WhoisBsa <mbscbsjbs@gmail.com>
2019-04-22 10:39:54 -0300

    HUGO

--- content/posts/my-first-post.md
+++
@@ -1,5 +0,0 @@
----
-title: "My First Post"
-date: 2019-04-17T22:35:04-03:00
-draft: true
----
---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 300) {
+        $(".fixed-top").addClass("change-fixed-top");
+    } else {
+        $(".fixed-top").removeClass("change-fixed-top");
+    }
+});
+
+$(document).on('click', 'a', function(event){
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 1250);
+    $('html, body').animate({path : new $.path.bezier(bezier_params)})
+
+});
